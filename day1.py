78e3b91d9b7779a8739e701c4d2c7c3f08e23448
charithacvsr <162982659+charithacvsr@users.noreply.github.com>
2024-03-11 16:10:58 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,64 @@
+day 1
+bargained charcter
+*controls and data structure-abc language
+*strings and its formula-from c
+*regular expressions-pearl
+*expression handling-modukes
+*magic methods from -farton
+,,,
+print ("hello world")
+,,,
+
+
+import this
+print(this)
+
+
+
+import antigravity
+,,,
+----->what kind of language is python
+*object oriented programming language
+*dynamically typed language
+*interpreted language
+*complied language
+*functional programming language
+------> flavours of python
+*cpython-written in c most common implementataion of python
+*jython-java implementation of python
+*iron python-implementation of the python programming language
+              which is slightly integrated with net
+*brython- browser python runs in browers
+*rubypython-bridge between python and rupy interpreters 
+*micropython-runs in micro controllers
+-----> print-print has 5 parameters
+print(object,sep,end,file,flush)
+
+**OBJECT------>the value to be printed is object
+#sep---->separator
+#eg
+a=8
+b=9
+c=89
+#print(a,b,c,sep='$')
+#end ----->which is used to string at the end of the value
+##it will keep the command to stay in same line
+#eg:
+#print(str(a),b,end='%%')
+#print(str(c))
+#!------>variables
+#?variable is aname which is used to identify the value underscores
+#?which is stored in aspecific memory location
+#!-----> rules to dedfine variables
+*variable have to start with only alphabets ,underscores
+*the char following the 1st char can be alphabets,numbers,underscores
+*no spaces are allowed
+*no special character like !@#$%^&*()+
+*case sensitive
+*keywords are not allowed as variables
+# ! ways to define variables
+# a=8
+# b=9
+
+
+
